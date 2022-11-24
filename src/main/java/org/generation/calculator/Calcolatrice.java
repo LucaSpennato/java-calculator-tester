8f035cb3e83eb2b97225f7903c593b5bbd00c767
Luca Spennato <luca.spennato@gmail.com>
2022-11-24 17:15:13 +0100

    feat/ calcolatrice class

---
+++ src/main/java/org/generation/calculator/Calcolatrice.java
@@ -0,0 +1,25 @@
+package org.generation.calculator;
+
+public class Calcolatrice {
+	
+	public float add(float num1, float num2){
+		return num1 + num2;
+	}
+
+	public float subtract(float num1, float num2) {
+		
+		if(num2 == 0) {
+			throw new IllegalArgumentException("Cant divide 0");
+		}
+		return num1 - num2;
+	}
+	
+	public float divide(float num1, float num2) {
+		return num1 / num2;
+	}
+	
+	public float multiply(float num1, float num2) {
+		return num1 * num2;
+	}
+	
+}
